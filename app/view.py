e40c99665a2507db4feea3a865c8195c0382bab4
边境 <bianjing@heqi-inc.com>
2019-12-23 19:11:07 +0800

    摘要

--- app/view.py
+++ app/view.py
@@ -9,6 +9,7 @@ from flask import render_template
 from flask import request, redirect, jsonify
 # from model.models import NewsChinese
 import logging, re
+import json
 # from train_model import *
 # import parse_text
 # from views import summarization_simple as ss
@@ -26,14 +27,29 @@ def index():
     return render_template('ProjectHome.html')
 
 
-@app.route('/SpeechExtraction/')
-def speech_extraction():
-    return render_template('SpeechExtraction.html')
+# @app.route('/SpeechExtraction/')
+# def speech_extraction():
+#     return render_template('SpeechExtraction.html')
+#
+#
+# @app.route('/SpeechExtraction/solve', methods=['GET', 'POST'])
+# def speech_extraction_solve():
+#     input_text = request.form.get('data')
+#     print(input_text)
+#     return render_template('SpeechExtraction.html')
+
+@app.route('/AbastractGeneration/')
+def generate_summarize():
+    return render_template('GenerateSummarize.html')
 
 
-@app.route('/SpeechExtraction/solve', methods=['GET', 'POST'])
-def speech_extraction_solve():
-    input_text = request.form.get('data')
-    print(input_text)
-    return render_template('SpeechExtraction.html')
+@app.route('/AbastractGeneration/solve', methods=['GET', 'POST'])
+def generate_summarize_solve():
+    data = request.json
+    text = data['text']
+    title = data['title']
+    num = data['num']
+    return render_template('GenerateSummarize.html')
+
+
 
