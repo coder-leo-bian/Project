3206b9692bff54b5b051da8bc4b96c3a5db2267c
bianjing2018 <bianjing166@163.com>
2020-01-03 09:12:06 +0800

    seq2seq

--- app/GenerationSummarize/seq2seq.py
+++ app/GenerationSummarize/seq2seq.py
@@ -1,11 +1,11 @@
 from keras.models import Model
 from keras.layers import LSTM, Embedding, Input, Dense
 from keras.preprocessing.sequence import pad_sequences
+import jieba
 import numpy as np
 HIDDEN_UNITS = 128
 
-
-
+"""<BOS> 开始字符，<EOS> 终止字符，<PAD> 1 补全字符， <UNK> 0 未知字符"""
 
 class Seq2Seq:
 
@@ -31,6 +31,7 @@ class Seq2Seq:
 
     def encoder(self):
         encoder_inputs = Input(shape=(None,), name='encoder_name')  # [32, 500] batch_size = 32, max_len=500
+        # 输入(batch, input_length) 输出 (batch, output_dim)
         encoder_embedding = Embedding(input_dim=self.num_input_tokens,
                                       output_dim=self.max_target_seq_length,
                                       input_length=self.max_input_seq_length)(encoder_inputs)  # [32, 50]
@@ -39,17 +40,17 @@ class Seq2Seq:
         encoder_outputs, encoder_state_h, encoder_state_c = encoder_lstm(encoder_embedding)
         encoder_states = [encoder_state_h, encoder_state_c]
         self.encoder_model = Model(encoder_inputs, encoder_states)
-        self.encoder_inputs= encoder_inputs
+        self.encoder_inputs = encoder_inputs
 
     def decoder(self):
         # decoder输入
-        decoder_inputs = Input(shape=(None, self.num_target_tokens), name='decoder_inputs')
+        decoder_inputs = Input(shape=(None, self.max_target_seq_length), name='decoder_inputs')
         # decoder 使用的循环神经网络
         decoder_lstm = LSTM(units=HIDDEN_UNITS, return_state=True,
                             return_sequences=True, name='decoder_lstm')
         decoder_outputs, decoder_state_h, decoder_state_c = decoder_lstm(decoder_inputs)
         # 全连接层
-        decoder_dense = Dense(units=self.num_target_tokens, activation='softmax',
+        decoder_dense = Dense(units=self.max_target_seq_length, activation='softmax',
                                 name='decoder_dense')
         decoder_outputs = decoder_dense(decoder_outputs)
 
@@ -66,26 +67,51 @@ class Seq2Seq:
 
     @staticmethod
     def get_config_path(model_dir_path):
-        return model_dir_path + '/' + Seq2Seq.model_name + '-config.npy'
+        return model_dir_path+ '/' + Seq2Seq.model_name + '-config.npy'
 
     @staticmethod
     def get_weight_path(model_dir_path):
         return model_dir_path + '/' + Seq2Seq.model_name + '-weight.h5'
 
-    def content_encoding(self):
-        pass
+    def content_encoding(self, contents):
+        # 1. 切词 [word1, word2, word3, ...]; 2. word to idx
+        X_contents = []
+        unk = 0
+        for content in contents:
+            X_content = []
+            content = jieba.lcut(content) if jieba.lcut(content) <= self.num_input_tokens else jieba.lcut(content)[: self.num_input_tokens+1]
+            for c in content:
+                if c in self.input_word2idx:
+                    X_content.append(self.input_word2idx[c])
+                else:
+                    X_content.append(unk)
+            X_contents.append(X_content)
+        return np.array(X_contents)
 
-    def target_encoding(self):
-        pass
+    def target_encoding(self, targets):
+        X_targets = []
+        unk = 0
+        for target in targets:
+            X_target = []
+            target = ['<BOS>'] + jieba.lcut(target) \
+                if len(jieba.lcut(target)) <= self.num_target_tokens - 2 \
+                else jieba.lcut(target)[: self.num_target_tokens - 1] + ['<EOS>']
+            for t in target:
+                if t in self.target_word2idx:
+                    X_targets.append(self.target_word2idx[t])
+                else:
+                    X_target.append(unk)
+            X_targets.append(X_target)
+        return np.array(X_targets)
 
     def fit(self, X_train, Y_train, X_test, Y_test, batch_size=32, epochs=10, model_dir_path=None):
         # 1. 获取模型路径和权重路径并保存
         config_file_path = Seq2Seq.get_config_path(model_dir_path)
         weight_file_path = Seq2Seq.get_weight_path(model_dir_path)
         np.save(config_file_path. self.config)
-
         # 2. input 和 target 文本处理(输入长度 <eos> <bos>)
-
+        X_train = self.content_encoding(X_train)
+        X_test = self.content_encoding(X_test)
         # 3. generate_batch
         # 4. fit_generator
         # 5. 保存权重
