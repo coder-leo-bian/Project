293629cf25bd941b3f4fd29acb84ed1327716db1
边境 <bianjing@heqi-inc.com>
2020-01-10 18:34:27 +0800

    attention summary

--- app/GenerationSummarize/attention.py
+++ app/GenerationSummarize/attention.py
@@ -1,11 +1,12 @@
 from keras import Model, Input, Sequential
 from keras.layers import LSTM, Embedding, Dense, Bidirectional, \
-    RepeatVector, Activation, Concatenate, Dot, Lambda
+    RepeatVector, Activation, Concatenate, Dot, Lambda, Softmax
 import numpy as np
 from keras import backend as K
 
 
 HIDDEN_UNIT = 128
+MAX_INPUT_SEQ_LENGTH = 500
 
 
 class Encoder:
@@ -26,47 +27,49 @@ class Encoder:
 
 
 class Attention:
-    at_dot = Dot(axes=1)
     def __init__(self, attention_size=5):
+        # self.model = Sequential([
+        #     Dense(attention_size, input_shape=(64, 500, 296)),
+        #     Activation('tanh'),
+        #     Dense(1),
+        # ])
         self.model = Sequential()
-        self.model.add(Dense(attention_size, activation='tanh'))
+        self.model.add(Dense(attention_size, activation='tanh', input_dim=()))
         self.model.add(Dense(1))
 
-    def forward(self, h, encoder_outputs):
-        repeat = RepeatVector(500)(h)
-        input_c_and_output_s = Concatenate(axis=-1)([encoder_outputs, repeat])
-        self.model(input_c_and_output_s)
-        attention = Activation('softmax')(input_c_and_output_s)
-        context = Attention.at_dot([attention, encoder_outputs])
-        return context
-
-    def attention_layer(self, encoder_outputs, max_target_seq_length=50):
-        lstm_layer = LSTM(HIDDEN_UNIT, return_state=True, name='at_LSTM_attention_layer')
-        h = Lambda(lambda X: K.zeros(shape=(K.shape(X)[0], HIDDEN_UNIT)), name='h_attention_layer')(encoder_outputs)
-        c = Lambda(lambda X: K.zeros(shape=(K.shape(X)[0], HIDDEN_UNIT)), name='c_attention_layer')(encoder_outputs)
-        output = []
-        for _ in range(max_target_seq_length):
-            context = self.forward(h, encoder_outputs)
-            h, _, c = lstm_layer(context, initial_state=[h, c])
-            output.append(h)
-        return output
+    def attention_mechanism(self, encoder_context_state, decoder_hidden_state):
+        """
+        :param model:
+        :param encoder_context_state: shape(64, 500, 256)
+        :param decoder_hidden_state: shape(64, 50) 广播500次
+        :return: 加权平均后的背景向量
+        """
+        # 对时间步序列进行广播
+        decoder_hidden_state = RepeatVector(MAX_INPUT_SEQ_LENGTH)(decoder_hidden_state)
+        # 对最后一列进行拼接
+        enc_and_dec_state = Concatenate(axis=-1)([decoder_hidden_state, encoder_context_state])
+        e = self.model(enc_and_dec_state)
+        # 对时间步进行softmax
+        alpha = Softmax(axis=1)(e)
+        new_context = alpha * encoder_context_state
+        return new_context
 
 
 class Decoder:
     def __init__(self, num_target_tokens):
         # 64, 50, 2000
-        self.inputs = Input(shape=(None, num_target_tokens), name='decoder_input')
-        self.attention = Attention()
+        self.inputs = Input(shape=(None, ), name='decoder_input')
         self.rnn = LSTM(HIDDEN_UNIT, return_state=True, name='decoder_lstm')
         self.dense = Dense(num_target_tokens, activation='softmax',name='decoder_dense')
 
-    def forward(self, encoder_outputs):
-        attention_outputs = self.attention.attention_layer(encoder_outputs)
-        decoder_outputs = [self.dense(timestep) for timestep in attention_outputs]
-        return decoder_outputs
+    def forward(self, encoder_outputs, decoder_inputs):
+        attention_outputs = Attention().attention_mechanism(encoder_outputs, decoder_inputs)
+        rnn_layer = self.rnn(attention_outputs)
+        dense_layer = self.dense(rnn_layer)
+        return dense_layer
 
 
-if __name__ == '__main__':
+def build_model():
     config = {'num_input_tokens': 5000, 'max_input_seq_length': 500, 'num_target_tokens': 2000}
     encoder = Encoder(config['num_input_tokens'], config['max_input_seq_length'])
     encoder_inputs = encoder.inputs
@@ -74,10 +77,13 @@ if __name__ == '__main__':
 
     decoder = Decoder(config['num_target_tokens'])
     decoder_inputs = decoder.inputs
-    decoder_outputs = decoder.forward(encoder_outputs)
-
+    decoder_outputs = decoder.forward(encoder_outputs, decoder_inputs)
     model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
     print(model.summary())
+
+if __name__ == '__main__':
+    build_model()
 
 
 
